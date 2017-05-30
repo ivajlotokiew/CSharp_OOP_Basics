6829f55a58b7d62fc9e0967de1da42d0dc030353
ivajlotokiew <ivajlo_nick@abv.bg>
2017-05-30 18:10:51 +0300

    Next Tasks

---
+++ Defining Classes/Define a Class Person/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+
+namespace Define_a_Class_Person
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type personType = typeof(Person);
+            FieldInfo[] fields = personType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+            Console.WriteLine(fields.Length);
+
+            Person personOne = new Person();
+            Person personTwo = new Person();
+            Person personThree = new Person();
+
+            personOne.name = "Pesho";
+            personOne.age = 20;
+            personTwo.name = "Gosho";
+            personTwo.age = 18;
+            personThree.name = "Stamat";
+            personThree.age = 43;
+        }
+    }
+}
